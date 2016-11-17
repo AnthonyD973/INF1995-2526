a6d39dd887d57c96d03479beb02c55a8945b1741
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2016-11-17 12:28:20 -0500

    Projet: Ajout de la classe CAN et ajout du squelette de la classe DistSnsr et ses commentaires.

---
+++ projet/src/DistSnsr.cpp
@@ -0,0 +1,23 @@
+/*
+ * Ecole Polytechnique de Montreal
+ * Departement de genie informatique
+ * Cours inf1995
+ *
+ * Emir Khaled Belhaddad, Anthony Dentinger,
+ * Gergi Younis et Vincent Dandenault
+ * 2016
+ *
+ * Code qui n'est sous aucune license.
+ *
+ */
+
+#include "DistSnsr.h"
+
+void DistSnsr::init(PinAPos pinANum) {
+    //TODO Implementer l'initialisation de la methode
+}
+
+uint8_t DistSnsr::read() {
+    //TODO Implementer la lecture de la valeur analogique et
+    //TODO le calcul de la distance.
+}
