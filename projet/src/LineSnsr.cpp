e1abce765f1633723e3d02dbc18723570a96ac47
gyounis <younisgeorges@gmail.com>
2016-11-17 23:27:07 -0500

    Projet : Ajout des fichiers LineSnsr.h et LineSnsr.cpp de la classe LineSnsr

---
+++ projet/src/LineSnsr.cpp
@@ -0,0 +1,23 @@
+/*
+ * Ecole Polytechnique de Montreal
+ * Département de Génie informatique
+ * Cours INF1995
+ *
+ * Emir Khaled Belhaddad, Anthony Dentinger,
+ * Gergi Younis et Vincent Dandenault
+ * 2016
+ *
+ * Code qui n'est sous aucune license.
+ *
+ */
+
+#include "LineSnsr.h"
+
+void LineSnsr::init(volatile uint8_t* port, uint8_t timerNum) {
+    //TODO Implémenter l'initialisation de la méthode
+}
+
+uint8_t LineSnsr::read() {
+    //TODO Implémenter la lecture des bits sur le port C
+    
+}
