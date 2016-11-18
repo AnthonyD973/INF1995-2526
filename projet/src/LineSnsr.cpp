4786f813deb3963af052cdd0d118a5a6be392419
gyounis <gergi.younis@polymtl.ca>
2016-11-18 17:00:45 -0500

    Projet : Mise a jour de la classe LineSnsr (codes et commentaires)

--- projet/src/LineSnsr.cpp
+++ projet/src/LineSnsr.cpp
@@ -13,12 +13,14 @@
 
 #include "LineSnsr.h"
 
-void LineSnsr::init(volatile uint8_t* port) {
-    //TODO Implémenter l'initialisation de la méthode
+void LineSnsr::init(volatile uint8_t* pin, volatile uint8_t* port, volatile uint8_t* ddr) {
+    _PIN = pin; // choix des pins pour recevoir les sorties du capteur
+    *ddr &= ~(_BV(0) | _BV(1) | _BV(2) | _BV(3) | _BV(4)); // port configuré en entrée pour recevoir les sorties du capteur sur les broches 1 à 5
+    *ddr |= _BV(5);  // port configuré en sortie et broche 6 mise à 1 pour ne pas se mettre en mode calibration
+    *port |= _BV(5); // 
 }
 
-uint8_t LineSnsr::read() {
-    //TODO Implémenter la lecture des bits sur le port C
-    return 0; //FIXME Valeur de retour pour que le code compile.
+uint8_t LineSnsr::read() { 
+    return *_PIN & 0x1F; // lecture de la sortie du capteur sur les cinq bits les moins significatifs 
     
 }
