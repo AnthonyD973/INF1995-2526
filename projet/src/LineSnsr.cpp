e605191032c72445cb8946fd864c6530b18ac517
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-18 09:10:50 -0500

    Projet: Whoops, hier j'ai fait des commit qui ne marchent pas sur la      branche master au lieu de la branche ColorSnsrTests. Retour en      arrière tout en gardant les changements de Gergi.

---
+++ projet/src/LineSnsr.cpp
@@ -0,0 +1,24 @@
+/*
+ * Ecole Polytechnique de Montreal
+ * Département de Génie informatique
+ * Cours INF1995
+ *
+ * Emir Khaled Belhaddad, Anthony Dentinger,
+ * Gergi Younis et Vincent Dandenault
+ * 2016
+ *
+ * Code qui n'est sous aucune license.
+ *
+ */
+
+#include "LineSnsr.h"
+
+void LineSnsr::init(volatile uint8_t* port, uint8_t timerNum) {
+    //TODO Implémenter l'initialisation de la méthode
+}
+
+uint8_t LineSnsr::read() {
+    //TODO Implémenter la lecture des bits sur le port C
+    return 0; //FIXME Valeur de retour pour que le code compile.
+    
+}
---
+++ projet/src/LineSnsr.h
@@ -0,0 +1,79 @@
+/* 
+ * Classe permettant l'accès au capteur suiveur de ligne LSS05 de Cytron Technologies.
+ *
+ * Ecole Polytechnique de Montreal
+ * Département de Génie Informatique
+ * Cours INF1995
+ *
+ * Emir Khaled Belhaddad, Anthony Dentinger,
+ * Gergi Younis et Vincent Dandenault
+ * 2016
+ *
+ * Code qui n'est sous aucune license.
+ *
+ */
+
+#ifndef LINE_SNSR_H
+#define LINE_SNSR_H
+
+#include <incl.h>
+#include <lib.h>
+
+
+#define SENSOR_PORT		PORTC
+#define SENSOR_DIRECTION	DDRC
+
+/**
+ * @class   LineSnsr
+ * 
+ * @brief   Classe qui permet de lire les valeurs binaires du capteur LSS05 
+ *          Une combinaison binaire de 5 bits en provenance du capteur indique
+ *          la position du robot par rapport à une ligne noire sur une surface blanche.
+ * 	    Cette indication sert à vérifier si le robot se trouve toujours sur la ligne
+ *	    et à corriger sa position dans le cas contraire 	
+ */
+class LineSnsr {
+
+public:
+    /**
+     * @brief   Initialisation du détecteur. À appeler au début du programme.
+     *		Met en entrée les broches du port C qui reçoit les sorties du capteur
+     *          Met en entrée le numéro du compteur qui sera utilisé pour générer des 
+     *          interruptions à une fréquence de 100 Hz afin de mettre à jour la lecture
+     * 		du capteur
+     *
+     * @param[in] port, timerNum 
+     */
+    static void init(volatile uint8_t* port, uint8_t timerNum);
+
+    /**
+     * @brief 	Lit la valeur du capteur pour donner la position    
+     * 	      	du robot par rapport à ligne qu'il suit
+     * @return  Un byte dont cinq bits sont d'intérêt. Deux bits du milieu servent
+     *		d'indicateur pour un déplacement suivant la ligne, i.e. 0b00110 ou 0b01100
+     *		Toute combinaison différente nécessite une correction 
+     */		
+    static uint8_t read();
+
+// Une méthode estSurLaligne() qui retourne un booléen pourrait être également intéressante; son résultat déclencherait l'appel de l'algorithme de correction de trajectoire ...
+
+private:
+    /**
+     * @type    uint8_t
+     * @brief   Numéro du compteur utilisé pour la génération d'interruption
+     */
+    static uint8_t _TIMER_NUM;
+    /**
+     * @type    uint8_t*
+     * @brief   Nom du port utilisé pour la réception des sorties du capteur
+     */
+    static uint8_t _PORT;
+    /**
+     * @type    Timer*
+     * @brief   Compteur utilisé.
+     */
+	static Timer* _TIMER;
+
+};
+
+#endif  // LINE_SNSR_H
