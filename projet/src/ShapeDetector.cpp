10276824f0d8426be2bd4589e06de7db990cba12
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-22 15:59:03 -0500

    Projet: Ajout de 'ShapeDetector.cpp'.

---
+++ projet/src/ShapeDetector.cpp
@@ -0,0 +1,17 @@
+#include "ShapeDetector.h"
+
+bool ShapeDetector::creaseCount_ = 0;
+const uint8_t ShapeDetector::uncert_ = 5;
+bool ShapeDetector::wasDecreasing_ = false;
+uint8_t ShapeDetector::prevVal_ = 0;
+
+Shape ShapeDetector::checkShape() {
+    
+}
+
+bool ShapeDetector::checkDirect_() {
+    
+}
+void ShapeDetector::updateCreaseCount_(bool isDecrasing) {
+    
+}
