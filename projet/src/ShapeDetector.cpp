087aec405ed8a740653b2dd344a9a1df06bcfef6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-22 16:36:59 -0500

    Projet: Début d'implémentation de 'ShapeDetector::checkShape'.

--- projet/src/ShapeDetector.cpp
+++ projet/src/ShapeDetector.cpp
@@ -1,17 +1,46 @@
 #include "ShapeDetector.h"
+#include "LineSnsr.h"
+#include "path.h"
 
-bool ShapeDetector::creaseCount_ = 0;
+uint8_t ShapeDetector::creaseCount_ = 0;
 const uint8_t ShapeDetector::uncert_ = 5;
-bool ShapeDetector::wasDecreasing_ = false;
-uint8_t ShapeDetector::prevVal_ = 0;
+bool ShapeDetector::wasDecreasing_ = true;
+uint8_t ShapeDetector::prevDist_ = 0;
 
 Shape ShapeDetector::checkShape() {
+    // initialiser
+    creaseCount_ = 0;
+    wasDecreasing_ = true;
+    prevDist_ = 0;
     
+    Path::turn(ROT_RIGHT, 0x7F);
+    
+    _delay_ms(200.0); // On veut s'assurer que l'on est sorti de la ligne.
+    
+    // On continue tant que l'on n'a pas détecté la ligne.
+    while (LineSnsr::read() < 0x10) {
+        bool isDecreasing = checkDecreasing_();
+        updateCreaseCount_(isDecreasing);
+    }
+    
+    Path::stop();
+    
+    // Prise de décision
+    Shape shape;
+    if (creaseCount_ <= 1) {
+        shape = CIRCLE;
+    } else if (creaseCount_ >= 4) {
+        shape = OCTOGON;
+    } else {
+        shape = SQUARE;
+    }
+    
+    return shape;
 }
 
-bool ShapeDetector::checkDirect_() {
+bool ShapeDetector::checkDecreasing_() {
     
 }
-void ShapeDetector::updateCreaseCount_(bool isDecrasing) {
+void ShapeDetector::updateCreaseCount_(bool isDecreasing) {
     
 }
--- projet/src/ShapeDetector.h
+++ projet/src/ShapeDetector.h
@@ -6,22 +6,24 @@ typedef uint8_t Shape;
 #include "DistSnsr.h"
 
 // ---Shape---
-#define CIRCLE  0
-#define OCTOGON 1
-#define SQUARE  2
+#ifndef CIRCLE_G
+    #define CIRCLE_G  0
+    #define OCTOGON_R 1
+    #define SQUARE_B  2
+#endif // CIRCLE_G
 
 class ShapeDetector {
 public:
     static Shape checkShape();
     
 private:
-    static bool checkDirect_();
-    static void updateCreaseCount_(bool isDecrasing);
+    static bool checkDecreasing_();
+    static void updateCreaseCount_(bool isDecreasing);
     
     static uint8_t creaseCount_;
     static const uint8_t uncert_;
     static bool wasDecreasing_;
-    static uint8_t prevVal_;
+    static uint8_t prevDist_;
 };
 
 #endif // SHAPE_DETECTOR_H
