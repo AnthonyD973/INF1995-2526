91e44d75c0855dc5aec8b239a25f5f806e2d9a30
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-25 18:28:06 -0500

    Lib: Résolution d'un bug assez bête dans lib.cpp.

--- lib/lib.cpp
+++ lib/lib.cpp
@@ -40,7 +40,7 @@ volatile uint8_t* getPortPtr(PinPosition pinPos) {
     switch (pinPos / 8) {
         case 0:  return &PORTA; break;
         case 1:  return &PORTB; break;
-        case 2:  return &PORTB; break;
+        case 2:  return &PORTC; break;
         default: return &PORTD; break;
     }
 }
@@ -49,7 +49,7 @@ uint8_t getPinVal(PinPosition pinPos) {
     switch (pinPos / 8) {
         case 0:  return PINA; break;
         case 1:  return PINB; break;
-        case 2:  return PINB; break;
+        case 2:  return PINC; break;
         default: return PIND; break;
     }
 }
