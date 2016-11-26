d7d3ca8ca6b84cd164310061dc4b461cd2aee940
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-26 17:39:12 -0500

    Projet: Refactoring du main.cpp de src/transfert.

--- projet/src/transfert/main.cpp
+++ projet/src/transfert/main.cpp
@@ -14,40 +14,45 @@
  *
  */
 
-#include "lib.h"
-#include "uart.h"
-#include "ram.h"
-#include "led.h"
+#include <lib.h>
+#include <uart.h>
+#include <ram.h>
+#include <led.h>
 
 int main() {
+    // Initialisation.
     uint16_t size = 0;
     uint8_t  buffer = 0;
+    
+    LED led(C0_C1, nullptr); // FIXME: Où est placé la LED, déjà?
     
     DDRA = DDRB = DDRC = 0xff;
     
     UART::init(2400);
     RAM::init();
     
-    PORTB = LED_RED;
     
-    for (uint8_t i = 0; i < 2; ++i) {
-        buffer = UART::receive();
-        size += (buffer << (8*(i)));
-    }
+    // Recevoir taille à écrire en mémoire.
+    led.setColor(LED_RED);
     
+    UART::receive((uint8_t*)&size, 2);
     RAM::write(0, (uint8_t*)&size, 2);
     
-    PORTB = LED_GREEN;
+    led.setColor(LED_GREEN);
     
+    
+    // Recevoir données.
     for (uint16_t i = 2; i < size; ++i) {
-        PORTB = LED_RED;
+        led.setColor(LED_RED);
         buffer = UART::receive();
-        PORTB = LED_GREEN;
+        led.setColor(LED_GREEN);
         RAM::write(i, buffer);
     }
     
+    
+    // Terminé.
     for EVER {
-        PORTB = LED_OFF;
+        led.setColor(LED_OFF);
     }
     
     return 0;
