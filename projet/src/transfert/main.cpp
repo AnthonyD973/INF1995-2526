928b219b57323b0afa1ed9e9578814c3fa8b9cc5
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-26 17:26:08 -0500

    Projet: Uniformisation du header du main.cpp de src/transfert.

--- projet/src/transfert/main.cpp
+++ projet/src/transfert/main.cpp
@@ -1,3 +1,19 @@
+/*
+ * Fichier d'entrée du programme permettant le chargement de données dans la
+ * mémoire du robot.
+ *
+ * Ecole Polytechnique de Montreal
+ * Departement de genie informatique
+ * Cours inf1995
+ *
+ * Emir Khaled Belhaddad, Anthony Dentinger,
+ * Gergi Younis et Vincent Dandenault
+ * 2016
+ *
+ * Code qui n'est sous aucune license.
+ *
+ */
+
 #include "lib.h"
 #include "uart.h"
 #include "ram.h"
