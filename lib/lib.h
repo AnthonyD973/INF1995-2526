e7f44cc927232c5e1e0f38c2e6d1765d85ed1642
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-28 15:35:42 -0500

    Lib: Uniformisation des headers de lib.h pour Doxygen.

--- lib/lib.h
+++ lib/lib.h
@@ -1,23 +1,22 @@
-/*
- * Fichier de symboles divers utilisés dans la librarie.
- *
- * Ecole Polytechnique de Montreal
- * Departement de genie informatique
- * Cours inf1995
- *
- * Emir Khaled Belhaddad, Anthony Dentinger,
- * Gergi Younis et Vincent Dandenault
- * 2016
- *
- * Code qui n'est sous aucune license.
- *
+/**
+ * @file    lib.h
+ * 
+ * @brief   Fichier de symboles divers utilisés dans la librarie..
+ * 
+ * @authors Belhaddad, Emir Khaled
+ * @authors Dandenault, Vincent
+ * @authors Dentinger, Anthony
+ * @authors Younis, Gergi
+ * 
+ * @date Automne 2016
+ * 
+ * @copyright Code qui n'est sous aucune license.
  */
 
 #ifndef LIB_H
 #define LIB_H
 
 #include "incl.h"
-
 #include "timer.h"
 #include "led.h"
 
@@ -25,14 +24,13 @@
 // =   SYMBOLES GLOBAUX DIVERS   =
 // ===============================
 
+/** @cond FALSE */ // Ne pas documenter ces macros.
 #define EVER (;;) // for EVER { ... }
 
-#ifndef NULL
-#define NULL 0
-#endif
 #ifndef nullptr
 #define nullptr 0
 #endif
+/** @endcond */
 
 
 #endif // LIB_H
