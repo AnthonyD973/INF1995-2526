e2f861d7727f785da78da1d260079795b9896a2b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-27 20:01:39 -0500

    Projet: Refactoring majeur de Path.

--- projet/src/path.cpp
+++ projet/src/path.cpp
@@ -1,160 +1,71 @@
 #include "path.h"
 
-uint16_t Path::pathAddr_[NB_PATHS];
-Engine*  Path::engL_ = NULL;
-Engine*  Path::engR_ = NULL;
+const PathState
+    S_BEGIN   = 0x1F, // Etat de début
+    S_FOR1    = 0x04, // Etat avancement
+    S_FOR2    = 0x0E,
+    S_FOR3    = 0x0A,
+    S_COR_R1  = 0x0C, // Etat correction vers la droite
+    S_COR_R2  = 0x08,
+    S_COR_L1  = 0x06, // Etat correction vers la gauche
+    S_COR_L2  = 0x02,
+    S_FCOR_R1 = 0x18, // Etat correction forcee droite
+    S_FCOR_R2 = 0x10,
+    S_FCOR_L1 = 0x03, // Etat correction forcee gauche
+    S_FCOR_L2 = 0x01,
+    S_B_R1    = 0x1C, // Etat Branchement à droite
+    S_B_R2    = 0x1E,
+    S_B_L1    = 0x07, // Etat Branchement à gauche
+    S_B_L2    = 0x0F,
+    S_STOP    = 0x1B; // Etat arret
 
-/**
- * @brief   Vérifie s'il le robot est rendu à un branchement.
- * @param[in] input Valeur lue du détecteur de ligne.
- * @return  Si le robot est à un embranchement.
- */
-bool checkBranch(uint8_t input);
-/**
- * @brief   Vérifie si l'état reçu en paramètre est un état valide,
- *      c'est-à-dire, s'il s'agit d'un des états que l'on peut s'attendre à
- *      observer durant le parcours.
- * @param[in] input Valeur lue du détecteur de ligne.
- * @return  Si l'état présent est un état valide.
- */
-bool checkValidState(uint8_t input);
 
-const State
-    S_BEGIN     = 0x1F, // Etat de début
-    S_FOR1      = 0x04, // Etat avancement
-    S_FOR2      = 0x0E,
-    S_FOR3      = 0x0A,
-    S_COR_R1    = 0x0C, // Etat correction vers la droite
-    S_COR_R2    = 0x08,
-    S_COR_L1    = 0x06, // Etat correction vers la gauche
-    S_COR_L2    = 0x02,
-    S_FCOR_R1   = 0x18, // Etat correction forcee droite
-    S_FCOR_R2   = 0x10,
-    S_FCOR_L1   = 0x03, // Etat correction forcee gauche
-    S_FCOR_L2   = 0x01,
-    S_B_R1      = 0x1C, // Etat Branchement à droite
-    S_B_R2      = 0x1E,
-    S_B_L1      = 0x07, // Etat Branchement à gauche
-    S_B_L2      = 0x0F,
-    S_STOP      = 0x1B; // Etat arret
+Engine* Path::engL_ = nullptr;
+Engine* Path::engR_ = nullptr;
+volatile PathState Path::currPathState_ = S_STOP;
 
-// (VAL) * 256 / (MAX - MIN + 1)
-//  18   * 256 / (80  - 10  + 1) = 65 = 0x41
+const uint8_t Path::MIDDLE_DIST_ = 0x5E;
+uint16_t Path::pathAddrs_[NB_PATHS];
 
-// Distance que le capteur doit mesurer : ~18 cm
-static const uint8_t MIDDLE_DIST = 94;
-
-volatile State Path::etat = S_STOP;
-
-/**
- * @brief   Fonction pour la routine d'interruption qui est utilisé pour faire
- *          une sorte de 'multi-threading'.
- */
-void updateDirection(void) {
-    if (checkValidState(LineSnsr::read()))
-        Path::etat = State(LineSnsr::read());
-    
-    switch(Path::etat) {
-    // ---AVANCEMENT---
-    case S_BEGIN:   // fallthrough
-    case S_B_L1:    // fallthrough
-    case S_B_L2:    // fallthrough
-    case S_B_R1:    // fallthrough
-    case S_B_R2:    // fallthrough
-    case S_FOR1:    // fallthrough
-    case S_FOR2:    // fallthrough
-    case S_FOR3:    Path::engL_->setPower(ENG_FORWARD,V_MAX);Path::engR_->setPower(ENG_FORWARD,V_MAX); break;
-    // ---ARRÊT---
-    case S_STOP:    Path::engL_->setMode(ENG_OFF);Path::engR_->setMode(ENG_OFF); break;
-    // ---CORRECTION---
-    case S_COR_R1:  // fallthrough
-    case S_COR_R2:  Path::engL_->setPower(ENG_FORWARD,V_MAX);Path::engR_->setPower(ENG_FORWARD,V_MOY);break;
-    case S_COR_L1:  // fallthrough
-    case S_COR_L2:  Path::engL_->setPower(ENG_FORWARD,V_MOY);Path::engR_->setPower(ENG_FORWARD,V_MAX);break;
-    // ---CORRECTION FORCÉE---
-    case S_FCOR_R1: // fallthrough
-    case S_FCOR_R2: Path::engL_->setPower(ENG_FORWARD,V_MOY);Path::engR_->setPower(ENG_BACKWARD,V_MOY);break;
-    case S_FCOR_L1: // fallthrough
-    case S_FCOR_L2: Path::engL_->setPower(ENG_BACKWARD,V_MOY);Path::engR_->setPower(ENG_FORWARD,V_MOY);break;
-    // ---DÉFAUT---
-    default: Path::engL_->setPower(ENG_FORWARD,V_MAX);Path::engR_->setPower(ENG_FORWARD,V_MAX); break;
-    }
-}
 
 ISR(TIMER2_OVF_vect) {
-    updateDirection();
+    Path::updateDirection_();
 }
 
-bool checkBranch(uint8_t input) {
-    switch(input) {
-    case S_B_L1: // fallthrough
-    case S_B_L2: // fallthrough
-    case S_B_R1: // fallthrough
-    case S_B_R2: return true;
-    default: return false;
-    }
-}
 
-bool checkValidState(uint8_t input) {
-    switch(input) {
-    case S_BEGIN:   // fallthrough
-    case S_FOR1:    // fallthrough
-    case S_FOR2:    // fallthrough
-    case S_FOR3:    // fallthrough
-    case S_COR_R1:  // fallthrough
-    case S_COR_R2:  // fallthrough
-    case S_COR_L1:  // fallthrough
-    case S_COR_L2:  // fallthrough
-    case S_FCOR_R1: // fallthrough
-    case S_FCOR_R2: // fallthrough
-    case S_FCOR_L1: // fallthrough
-    case S_FCOR_L2: // fallthrough
-    case S_STOP:    // fallthrough
-    case S_B_L1:    // fallthrough
-    case S_B_L2:    // fallthrough
-    case S_B_R1:    // fallthrough
-    case S_B_R2: return true;
-    default: return false;
-    }
-}
+// ====================
+// =       PATH       =
+// ====================
 
+// PUBLIC
 
 void Path::init(Engine* engL, Engine* engR) {
     engL_ = engL;
     engR_ = engR;
     engL_->getTimer()->denyOVFI();
     RAM::init();
-    readPath_(0x0000);
+    populatePathAddrs_(0x0000);
 }
 
 void Path::doPath(uint8_t path) {
     uint8_t  instr;
-    uint16_t instrAddr = pathAddr_[path];
-    RAM::read(instrAddr, &instr);
+    uint16_t instrAddr = pathAddrs_[path];
+    
+    RAM::read(instrAddr++, &instr);
     while (instr != ENP_OP) {
-        UART::transmitHex(pathAddr_[path]);
-        UART::transmit(' ');
-        UART::transmitHex(instr);
-        UART::transmit('\n');
         switch (instr) {
-        case INI_OP: ini(); break;
-        case TNR_OP: tnr(); break;
-        case TNL_OP: tnl(); break;
-        case MDL_OP: mdl(); break;
-        default:  enp(); break;
+         case INI_OP: ini_(); break;
+         case TNR_OP: tnr_(); break;
+         case TNL_OP: tnl_(); break;
+         case MDL_OP: mdl_(); break;
+         default:     enp_(); break;
         }
-        RAM::read(++instrAddr, &instr);
+        RAM::read(instrAddr++, &instr);
     }
-    enp();
+    
+    enp_();
 }
 
-void Path::readPath_(uint16_t addr) {
-    uint16_t pathCount = 0; // Nombre de chemins dans le fichier.
-    RAM::read(addr+2, (uint8_t*)&pathCount, 2);
-    for(uint8_t i = 0; i < pathCount; ++i) {
-        RAM::read(addr + 4 + i*2, (uint8_t*)(&(pathAddr_[i])), 2);
-    }
-}
 
 void Path::forward() {
     engL_->getTimer()->allowOVFI();
@@ -168,65 +79,185 @@ void Path::stop() {
 
 void Path::turn(RotDir dir, uint8_t speed) {
     engL_->getTimer()->denyOVFI();
-    engL_->setPower( dir ? ENG_BACKWARD : ENG_FORWARD, speed);
-    engR_->setPower(!dir ? ENG_BACKWARD : ENG_FORWARD, speed);
+    engL_->setPower(dir == ROT_LEFT ? ENG_BACKWARD : ENG_FORWARD,  speed);
+    engR_->setPower(dir == ROT_LEFT ? ENG_FORWARD  : ENG_BACKWARD, speed);
 }
 
-// +=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=+
-// | instructions de l'interpréteur |
-// +=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=+
-
-void Path::ini(void) {
-    // Les moteur sont arrêtés tant que le robot n'est pas sur la ligne de 
-    // départ.
-    stop();
-    while (LineSnsr::read() != S_BEGIN);
-    // On le fait avancer juste assez pour qu'il quite la ligne de départ...
-    forward();
-    _delay_ms(500.0);
-    // puis on passe à l'instruction suivante
-    stop();
+void Path::updateDirection_() {
+    if (isValidState_(LineSnsr::read())) {
+        currPathState_ = PathState(LineSnsr::read());
+    }
+    
+    EngineMode engLMode, engRMode;
+    uint16_t   engLPwr,  engRPwr;
+    
+    switch(currPathState_) {
+     // ---AVANCEMENT---
+     case S_BEGIN:   // fallthrough
+     case S_B_L1:    // fallthrough
+     case S_B_L2:    // fallthrough
+     case S_B_R1:    // fallthrough
+     case S_B_R2:    // fallthrough
+     case S_FOR1:    // fallthrough
+     case S_FOR2:    // fallthrough
+     case S_FOR3:    engLMode = ENG_FORWARD;  engLPwr = V_MAX;
+                     engRMode = ENG_FORWARD;  engRPwr = V_MAX;
+                     break;
+     // ---ARRÊT---
+     case S_STOP:    engLMode = ENG_OFF;      engLPwr = V_MIN;
+                     engRMode = ENG_OFF;      engRPwr = V_MIN;
+                     break;
+     // ---CORRECTION---
+     case S_COR_R1:  // fallthrough
+     case S_COR_R2:  engLMode = ENG_FORWARD;  engLPwr = V_MAX;
+                     engRMode = ENG_FORWARD;  engRPwr = V_MOY;
+                     break;
+     case S_COR_L1:  // fallthrough
+     case S_COR_L2:  engLMode = ENG_FORWARD;  engLPwr = V_MOY;
+                     engRMode = ENG_FORWARD;  engRPwr = V_MAX;
+                     break;
+     // ---CORRECTION FORCÉE---
+     case S_FCOR_R1: // fallthrough
+     case S_FCOR_R2: engLMode = ENG_FORWARD;  engLPwr = V_MOY;
+                     engRMode = ENG_BACKWARD; engRPwr = V_MOY;
+                     break;
+     case S_FCOR_L1: // fallthrough
+     case S_FCOR_L2: engLMode = ENG_BACKWARD; engLPwr = V_MOY;
+                     engRMode = ENG_FORWARD;  engRPwr = V_MOY;
+                     break;
+     // ---DÉFAUT---
+     default:        engLMode = ENG_FORWARD;  engLPwr = V_MAX;
+                     engRMode = ENG_FORWARD;  engRPwr = V_MAX;
+                     break;
+    }
+    engL_->setPower(engLMode, engLPwr);
+    engR_->setPower(engRMode, engRPwr);
+    
 }
-void Path::tnr(void) {
+
+// PRIVATE
+
+bool Path::isBranch_(uint8_t input) {
+    switch(input) {
+     case S_B_L1: // fallthrough
+     case S_B_L2: // fallthrough
+     case S_B_R1: // fallthrough
+     case S_B_R2: return true;
+     default:     return false;
+    }
+}
+
+bool Path::isValidState_(uint8_t input) {
+    switch(input) {
+     case S_BEGIN:   // fallthrough
+     case S_FOR1:    // fallthrough
+     case S_FOR2:    // fallthrough
+     case S_FOR3:    // fallthrough
+     case S_COR_R1:  // fallthrough
+     case S_COR_R2:  // fallthrough
+     case S_COR_L1:  // fallthrough
+     case S_COR_L2:  // fallthrough
+     case S_FCOR_R1: // fallthrough
+     case S_FCOR_R2: // fallthrough
+     case S_FCOR_L1: // fallthrough
+     case S_FCOR_L2: // fallthrough
+     case S_STOP:    // fallthrough
+     case S_B_L1:    // fallthrough
+     case S_B_L2:    // fallthrough
+     case S_B_R1:    // fallthrough
+     case S_B_R2: return true;
+     default:     return false;
+    }
+}
+
+void Path::populatePathAddrs_(uint16_t fileBegAddr) {
+    uint16_t nPaths = 0;
+    RAM::read(fileBegAddr + 2, (uint8_t*)&nPaths, 2);
+    
+    for(uint8_t i = 0; i < nPaths; ++i) {
+        uint16_t addrToPathAddr = fileBegAddr + 4 + 2 * i;
+        RAM::read(addrToPathAddr, (uint8_t*)(pathAddrs_ + i), 2);
+    }
+}
+
+// ====================
+// =  INSTRUCTIONS DE = 
+// =  L'INTERPRÉTEUR  =
+// ====================
+
+void Path::ini_() {
+    stop();
+    while (LineSnsr::read() != S_BEGIN); // Attendre d'être sur la ligne de départ
+    
     forward();
-    _delay_ms(250.0);
-    while (!checkBranch(LineSnsr::read()));
+    _delay_ms(500.0); // Dépasser la ligne de départ avant la prochaine instruction
+                      // pour éviter de trouver des branchements là où il n'y
+    stop();           // en a pas.
+    
+}
+
+void Path::tnl_() {
+    forward();
+    _delay_ms(250.0); // S'assurer d'être stable sur la ligne pour éviter de
+                      // trouver des branchements là où il n'y en a pas.
+    
+    while (!isBranch_(LineSnsr::read())); // Attendre un branchement.
+    
     stop();
     engL_->setPower(ENG_FORWARD, V_MAX);
     engR_->setPower(ENG_FORWARD, V_MAX);
-    _delay_ms(1100.0);
-    turn(ROT_RIGHT, V_MAX);
-    _delay_ms(500.0);
-    while (!(LineSnsr::read() & 0x08));
-    forward();
-    _delay_ms(500.0);
-    stop();
-}
-void Path::tnl(void) {
-    forward();
-    _delay_ms(250.0);
-    while (!checkBranch(LineSnsr::read()));
-    stop();
-    engL_->setPower(ENG_FORWARD, V_MAX);
-    engR_->setPower(ENG_FORWARD, V_MAX);
-    _delay_ms(1100.0);
+    _delay_ms(1100.0); // S'avancer jusqu'à ce que les roues soient vis-à-vis
+                       // du branchement.
+    
     turn(ROT_LEFT, V_MAX);
-    _delay_ms(500.0);
-    while (!(LineSnsr::read() & 0x02));
+    _delay_ms(500.0); // Quitter la ligne
+    
+    while (!(LineSnsr::read() & 0x02)); // Attendre de trouver la ligne vers
+                                        // laquelle on tourne.
     forward();
-    _delay_ms(500.0);
+    _delay_ms(500.0); // S'assurer d'être stable sur la ligne pour éviter de
+                      // trouver des branchements là où il n'y en a pas.
     stop();
 }
-void Path::mdl(void) {
+
+void Path::tnr_() {
     forward();
-    _delay_ms(1200.0);
-    while (DistSnsr::readAverage() > MIDDLE_DIST);
+    _delay_ms(250.0); // S'assurer d'être stable sur la ligne pour éviter de
+                      // trouver des branchements là où il n'y en a pas.
+    
+    while (!isBranch_(LineSnsr::read())); // Attendre un branchement.
+    
+    stop();
+    engL_->setPower(ENG_FORWARD, V_MAX);
+    engR_->setPower(ENG_FORWARD, V_MAX);
+    _delay_ms(1100.0); // S'avancer jusqu'à ce que les roues soient vis-à-vis
+                       // du branchement.
+    
+    turn(ROT_RIGHT, V_MAX);
+    _delay_ms(500.0); // Quitter la ligne
+    
+    while (!(LineSnsr::read() & 0x08)); // Attendre de trouver la ligne vers
+                                        // laquelle on tourne.
+    
+    forward();
+    _delay_ms(500.0); // S'assurer d'être stable sur la ligne pour éviter de
+                      // trouver des branchements là où il n'y en a pas.
+    stop();
+}
+
+void Path::mdl_() {
+    forward();
+    _delay_ms(1200.0); // Dépasser la porte d'entrée dans la pièce.
+    
+    while (DistSnsr::readAverage() > MIDDLE_DIST_);
     engL_->setMode(ENG_BACKWARD);
     engR_->setMode(ENG_BACKWARD);
-    _delay_ms(300.0);
+    _delay_ms(300.0); // S'arrêter le plus vite possible.
+    
     stop();
-    _delay_ms(500.0);
+    _delay_ms(500.0); // Attendre la fin du mouvement.
 }
-void Path::enp(void) {
+
+void Path::enp_() {
     stop();
 }
--- projet/src/path.h
+++ projet/src/path.h
@@ -29,13 +29,13 @@
 #include "LineSnsr.h"
 #include "DistSnsr.h"
 
-typedef uint8_t State;
+typedef uint8_t PathState;
 
 // Constantes pour les vitesses
 const uint8_t
-    V_MAX = 0x8F,
-    V_MOY = 0x6F,
-    V_MIN = 0x00;
+    V_MAX = 0x8F, /**< @brief Puissance maximale à transmettre aux moteurs. */
+    V_MOY = 0x6F, /**< @brief Puissance moyenne à transmettre aux moteurs. */
+    V_MIN = 0x00; /**< @brief Puissance minimale à transmettre aux moteurs. */
 
 /**
  * @brief   Classe gérant tout ce qui a trait au positionnement du robot par
@@ -55,6 +55,14 @@ public :
      */
     static void init(Engine* engL, Engine* engR);
     /**
+     * @brief Lit les instructions en mémoire et effectue les actions
+     *      correspondantes. Voir Opcode.
+     * @param[in] path  Numéro du chemin en mémoire. Théoriquement, ce
+     *      paramètre pourrait avoir n'importe quelle valeur, mais, pour notre
+     *      projet, nous n'utilisons que 3 chemins, donc 0 ≤ path ≤ 2.
+     */
+    static void doPath(uint8_t path);
+    /**
      * @brief   Fait avancer le robot en suivant la ligne.
      */
     static void forward();
@@ -69,54 +77,108 @@ public :
      */
     static void turn(RotDir dir, uint8_t speed);
     /**
-     * @brief Lit les instructions en mémoire et effectue les actions
-     *      correspondantes. Voir Opcode.
-     * @param[in] path  Numéro du chemin en mémoire. Théoriquement, ce
-     *      paramètre pourrait avoir n'importe quelle valeur, mais, pour notre
-     *      projet, nous n'utilisons que 3 chemins, donc 0 ≤ path ≤ 2.
+     * @brief   Ajuste la puissance des moteurs dépendamment du
+     *      @link #currPathState_ @endlink actuel.
+     * 
+     * La routine d'interruption qui appelle cette fonction est utilisée pour
+     * faire une sorte de 'multi-threading'.
+     * @warning Bien que cette méthode soit publique, seules les routines
+     *      d'interruption devraient faire y faire référence.
      */
-    static void doPath(uint8_t path);
+    static void updateDirection_();
     
     
     /**
      * @brief   Moteur de gauche.
-     * @warning Ne pas utiliser, sauf dans les routines d'interruption.
+     * @warning Bien que cet attribut soit publique, seules les routines
+     *      d'interruption devraient faire y faire référence.
      */
     static Engine* engL_;
     /**
      * @brief   Moteur de droite.
-     * @warning Ne pas utiliser, sauf dans les routines d'interruption.
+     * @warning Bien que cet attribut soit publique, seules les routines
+     *      d'interruption devraient faire y faire référence.
      */
     static Engine* engR_;
     /**
      * @brief   État actuel du suivi de ligne, par exemple :
      *      'correction à gauche', 'avancement rectiligne'.
-     * @warning Ne pas utiliser, sauf dans les routines d'interruption.
+     * @warning Bien que cet attribut soit publique, seules les routines
+     *      d'interruption devraient faire y faire référence.
      */
-    static volatile State etat;
+    static volatile PathState currPathState_;
     
-private :
+private:
+    /**
+     * @brief   Vérifie s'il le robot est rendu à un branchement.
+     * @param[in] input Valeur lue du détecteur de ligne.
+     * @return  Si le robot est à un embranchement.
+     */
+    static bool isBranch_(uint8_t input);
+    /**
+    * @brief   Vérifie si l'état reçu en paramètre est un état valide,
+    *      c'est-à-dire, s'il s'agit d'un des états que l'on peut s'attendre à
+    *      observer durant le parcours.
+    * @param[in] input Valeur lue du détecteur de ligne.
+    * @return  Si l'état présent est un état valide.
+    */
+    static bool isValidState_(uint8_t input);
+    
+    /**
+     * @brief   Remplit le tableau pathAddr_ avec les adresses de début des
+     *      chemins.
+     * @param[in] fileBegAddr  Adresse de début du fichier généré par GAS.
+     */
+    static void populatePathAddrs_(uint16_t fileBegAddr);
+    
+    /**
+     * @brief Valeur que DistSnsr::read doit retourner lorsque le robot est au
+     *      millieu de la pièce.
+     */
+    static const uint8_t MIDDLE_DIST_;
+    
     /**
      * @brief   Tableau dont chaque élément est l'adresse de la première
      *          instruction d'un chemin.
      */
-    static uint16_t pathAddr_[NB_PATHS];
+    static uint16_t pathAddrs_[NB_PATHS];
+
+    // ====================
+    // =  INSTRUCTIONS DE = 
+    // =  L'INTERPRÉTEUR  =
+    // ====================
+    
     /**
-     * @brief   Remplit le tableau pathAddr_ avec les adresses de début des
-     *      chemins.
-     * @param[in] addr  Adresse de début du fichier généré par GAS.
+     * @brief   <b>INI</b>t
+     * 
+     * Attendre que les 5 capteurs de ligne soient actifs, puis se déplacer en
+     * avant pendant une demi-seconde.
      */
-    static void readPath_(uint16_t addr);
-    
-    // +=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=+
-    // | instructions de l'interpréteur |
-    // +=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=+
-    
-    static void ini(void);
-    static void tnr(void);
-    static void tnl(void);
-    static void mdl(void);
-    static void enp(void);
+    static void ini_();
+    /**
+     * @brief   <b>T</b>ur<b>N L</b>eft
+     * 
+     * Au prochain embranchement, tourner à gauche.
+     */
+    static void tnl_();
+    /**
+     * @brief   <b>T</b>ur<b>N R</b>ight
+     * 
+     * Au prochain embranchement, tourner à droite.
+     */
+    static void tnr_();
+    /**
+     * @brief   <b>M</b>i<b>D</b>d<b>L</b>e
+     * 
+     * Se mettre au milieu de la pièce.
+     */
+    static void mdl_();
+    /**
+     * @brief   <b>EN</b>d <b>P</b>ath
+     * 
+     * Le chemin est terminé ; les prochaines instructions sont pour le chemin suivant.
+     */
+    static void enp_();
 };
 
 
