447ef53597cd0f308e42b26e32ffa2afe25bf8c4
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-11-29 20:42:50 -0500

    Doxygen: Implémentation de la cible docs qui génère les 3 documentations.

--- projet/gas/main.cpp
+++ projet/gas/main.cpp
@@ -20,7 +20,7 @@
  * Notre répertoire Git contient trois documentations:
  * <ol type="1">
  *     <li>La documentation de notre [librairie](@ref LIB_MAINPAGE), située sous lib/ ;</li>
- *     <li>La documentation de notre [compilateur GAS](@ref LIB_MAINPAGE), situé sous projet/gas/; </li>
+ *     <li>La documentation de notre [compilateur GAS](@ref GAS_MAINPAGE), situé sous projet/gas/; </li>
  *     <li>La documentation des [fichiers créés pour notre projet](@ref SRC_MAINPAGE), situés sous projet/src.</li>
  * </ol>
  */
