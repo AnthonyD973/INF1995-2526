ac743d77a224ddabea5cd0f8041bae2022b87f06
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2016-12-02 17:26:06 -0500

    Doxygen: Changement des pages d'accueil pour utiliser la nouvelle cible      'everything'.

--- projet/src/main.cpp
+++ projet/src/main.cpp
@@ -26,6 +26,7 @@
  * 
  * <div id="PLUGIN">
  * <h2>Comment brancher les composantes</h2>
+ * </div>
  * <ul>
  *     <li>PORT A</li>
  *     <ul>
@@ -52,57 +53,29 @@
  *     </ul>
  * </ul>
  * 
+ * <div if="SNSRS">
+ * <h2>Placement des capteurs</h2>
  * </div>
+ * 
+ * <ul><li><b>@link LineSnsr Suiveur de lignes@endlink (LSS05)</b></li></ul>
+ * Le suiveur de lignes est placé sous le robot et en avant.
+ * 
+ * <ul><li><b>@link DistSnsr Capteur de distance@endlink (GP2D12)</b></li></ul>
+ * Le capteur de distance est placé sur le robot et en avant.
+ * 
+ * <ul><li><b>@link ColorSnsr Capteur de couleurs@endlink (TCS230)</b></li></ul>
+ * Le capteur de couleurs est placé sur le robot et sur la droite.
+ * 
  * 
  * <div id="HOWTO">
  * <h2>Faire fonctionner notre projet</h2>
  * </div>
  * 
- * Pour faire fonctionner le programme, il vous faut tout d'abord transférer le
- * bytecode des chemins dans la mémoire externe du robot afin que l'interpréteur
- * puisse les lire. Le programme a aussi besoin d'avoir la chanson à jouer
- * lorsque le robot a fini le parcours.
+ * Pour faire fonctionner votre robot, simplement exécuter la commande...
  * 
+ *     $ make everything
  * 
- * Pour transférer les chemins, ouvrez un terminal puis entrez les commandes:
- * 
- * <pre class="fragment">path/to/repo/<b>projet/gas$</b> make
- * path/to/repo/<b>projet/gas$</b> ./gas input.txt paths.bin; cp paths.bin ../src/transfert</pre>
- * 
- * Par la suite, assurez-vous que le robot est branché et
- * prêt à recevoir des données et exécutez la commande:
- *     
- * <pre class="fragment">path/to/repo/<b>projet/src/transfert$</b> make install</pre>
- * 
- * La Del libre devrait s'allumer en rouge. Ensuite, faites la
- * commande suivante pour transférer les chemins sur la @link RAM mémoire externe@endlink:
- * 
- * <pre class="fragment">path/to/repo/<b>projet/src/transfert$</b> serieViaUSB -e -h -s 8 -f paths.bin</pre>
- * 
- * La Del devrait clignoter faiblement avant de s'éteindre. Maintenant, les chemins
- * sont normalement transférés dans la mémoire et visibles pour l'interpréteur.
- * 
- * 
- * Pour transférer la chanson de fin, tout ce que vous devez faire est d'entrer la commande:
- * 
- * <pre class="fragment">path/to/repo/<b>projet/song$</b> make</pre>
- * 
- * Le processus est automatisé. La chanson qui se trouve dans le code du
- * sous-répertoire song_data/ est tout d'abord compilée en un fichier binaire pour
- * ensuite être transféré à l'aide du programme dans le répertoire projet/song/ .
- * Lors du transfert, la Del devrait clignotER entre vert et rouge puis, à la fin du
- * transfert, devrait s'éteindre.
- * 
- * 
- * Finalement, lorsque toutes les données ont été transférées dans la mémoire
- * externe, il ne reste plus qu'a envoyer le programme principal dans la mémoire
- * flash.
- * 
- * <pre class="fragment">path/to/repo/<b>projet/src</b><b>$</b> make install</pre>
- * 
- * Le programme se compile et s'installe automatiquement. Il ne reste plus qu'a
- * placer le robot sur la ligne de départ, enclancher les moteurs et démarrer le
- * robot!
+ * ...dans le dossier projet/src
  */
 
 #include <lib.h>
